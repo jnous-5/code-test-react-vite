efae648c1e6b95c537705f077f73cd77be21db6f
Jhoncel Nayve <jhoncelnayve@gmail.com>
2025-05-05 07:23:45 +0000

    Configure imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1",
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+    },
   },
 });
